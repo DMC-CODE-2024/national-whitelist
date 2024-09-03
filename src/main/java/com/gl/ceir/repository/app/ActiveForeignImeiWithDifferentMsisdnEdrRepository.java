1d6ddf8c30b02de1a4abb4f1fc72fbb5efa52ad0
radhay-1199 <radhay.1199@gmail.com>
2024-09-03 23:22:53 +0530

    fix: date update issues

--- src/main/java/com/gl/ceir/repository/app/ActiveForeignImeiWithDifferentMsisdnEdrRepository.java
+++ src/main/java/com/gl/ceir/repository/app/ActiveForeignImeiWithDifferentMsisdnEdrRepository.java
@@ -16,7 +16,7 @@ public interface ActiveForeignImeiWithDifferentMsisdnEdrRepository extends JpaRe
     @Query(value = "SELECT  a.*, CASE WHEN m.device_id IS NULL THEN 'false' ELSE 'true' END AS validity_flag, m.device_type FROM active_foreign_imei_with_different_imsi a LEFT JOIN mobile_device_repository m ON a.tac = m.device_id WHERE a.created_on <= :endDate AND a.created_on >= :startDate", nativeQuery = true)
     public Page<ActiveForeignImeiWithDifferentImsi> findAllLatestDifferentImeiInLastXDays(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, Pageable pageable);
 
-    @Query(value = "SELECT MIN(a.createdOn) FROM ActiveForeignImeiWithDifferentImsi a")
+    @Query(value = "SELECT min(created_on) from active_foreign_imei_with_different_imsi", nativeQuery = true)
     public String getEarliestActiveTimestamp();
 
     @Query(value = "SELECT a FROM ActiveForeignImeiWithDifferentImsi a WHERE a.createdOn BETWEEN :startDate AND :endDate")
--- src/main/java/com/gl/ceir/repository/app/ActiveUniqueForeignImeiEdrRepository.java
+++ src/main/java/com/gl/ceir/repository/app/ActiveUniqueForeignImeiEdrRepository.java
@@ -16,7 +16,7 @@ public interface ActiveUniqueForeignImeiEdrRepository extends JpaRepository<Acti
     @Query(value = "SELECT  a.*, CASE WHEN m.device_id IS NULL THEN 'false' ELSE 'true' END AS validity_flag, m.device_type FROM active_unique_foreign_imei a LEFT JOIN mobile_device_repository m ON a.tac = m.device_id WHERE a.created_on <= :endDate AND a.created_on >= :startDate", nativeQuery = true)
     public Page<ActiveUniqueForeignImeiEdr> findAllLatestUniqueImeiInLastXDays(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime date, Pageable pageable);
 
-    @Query(value = "SELECT MIN(a.createdOn) FROM ActiveUniqueForeignImeiEdr a")
+    @Query(value = "SELECT min(created_on) from active_unique_foreign_imei", nativeQuery = true)
     public String getEarliestActiveTimestamp();
 
     @Query(value = "SELECT a FROM ActiveUniqueForeignImeiEdr a WHERE a.createdOn BETWEEN :startDate AND :endDate")
